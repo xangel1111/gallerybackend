043d6c7daeb3b6fe8859a8acf01bb0ef5a1dbbf1
xangel1111 <angel.delgado@tecsup.edu.pe>
2025-12-22 21:39:51 -0500

    update cors

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    'https://galleryclient.vercel.app',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}));
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 
